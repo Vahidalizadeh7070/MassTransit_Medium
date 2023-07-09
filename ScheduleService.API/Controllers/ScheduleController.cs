62240c2e9ce2fa524cb87dac78b1274b361b0cf4
vahidalizadeh7070 <vahidalizadeh7070@gmail.com>
2023-07-09 15:40:31 +0330

    Add schedule controller to the Schedule project.

---
+++ ScheduleService.API/Controllers/ScheduleController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScheduleService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScheduleController : ControllerBase
+    {
+        public ScheduleController()
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            return Ok();
+        }
+    }
+}
