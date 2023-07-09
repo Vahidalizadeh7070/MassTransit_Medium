d3816a0d4979fe37e58b5602c18fbbd488fef213
vahidalizadeh7070 <vahidalizadeh7070@gmail.com>
2023-07-09 15:50:19 +0330

    Add CreateScheduleDTO object to get data from the clients through the schedule API endpoint.

---
+++ ScheduleService.API/DTO/ScheduleDTOs/CreateScheduleDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ScheduleService.API.DTO.ScheduleDTOs
+{
+    public class CreateScheduleDTO
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Title { get; set; }
+        public DateTime ScheduleDate { get; set; }
+        public bool IsActive { get; set; } = false;
+    }
+}
