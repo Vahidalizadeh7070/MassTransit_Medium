3ed8fb03c9cb01ee164de6198f9dd5a1688ef5c6
vahidalizadeh7070 <vahidalizadeh7070@gmail.com>
2023-07-09 16:34:21 +0330

    Add basic masstransit configuration in Program.cs file.

--- ScheduleService.API/Program.cs
+++ ScheduleService.API/Program.cs
@@ -1,6 +1,15 @@
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddMassTransit(m =>
+{
+    // If the API us going to use local host rabbitmq leave it like the below
+    // otherwise, use configration of your cloud services or etc. inside UsingRabbitMq()
+    m.UsingRabbitMq();
+});
 
 var app = builder.Build();
 
